d7c8427c2a94853b22eb7cb649ff30952df0d9ff
Troeborec <defied_jeff@mail.ru>
2023-02-05 01:34:32 +1200

    Gotovo!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
 public class Main {
+
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Car carOne = new Car("Lada",
+                "Granta",
+                1.7f,
+                "Желтый",
+                2015,
+                "Россия");
+
+        Car carTwo = new Car("Audi",
+                "A8",
+                2020,
+                "Черный",
+                2020,
+                "Германия");
+
+        Car carThree = new Car("BMW",
+                "Z8",
+                3.0f,
+                "Черный",
+                2021,
+                "Германия");
+
+        Car carFour = new Car("Kia",
+                "Sportage 4-го покаления",
+                2.4f,
+                "Красный",
+                2018,
+                "Южная Корея");
+
+        Car carFive = new Car("Hyundai",
+                "Avante",
+                1.6f,
+                "Оранжевый",
+                2016,
+                "Южная Корея");
+
+        System.out.println(carOne + " \n" + carTwo + "\n" + carThree + "\n" + carFour + "\n" + carFive);
     }
-}
+    }
